684aded5d553738a1b6a73f67028b632c74b701e
jiwon <bjiwon0324@gmail.com>
2025-05-27 15:56:35 +0900

    feat: falling leaves background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@
 import classNamesBind from "classnames/bind";
 import styles from "./page.module.scss";
 import {useFullPage} from "@/libs/hooks";
-import {fullPageObjArray} from "@/libs/constants/fullPageObjArray";
+import {FULL_PAGE_OBJ_ARRAY} from "@/libs/constants/fullPageObjArray";
 import Header from "@/components/(commons)/Header";
 
 const cx = classNamesBind.bind(styles);
 
 export default function Home() {
- const {wrapRef,sectionRefs,currentSectionIndex, scrollToSection} = useFullPage(fullPageObjArray)
+ const {wrapRef,sectionRefs,currentSectionIndex, scrollToSection} = useFullPage(FULL_PAGE_OBJ_ARRAY)
 
   return (
     <>
@@ -18,7 +18,7 @@ export default function Home() {
         className={cx("wrap")}>
                       <Header/>
         <main className={cx("main")}>
-          {fullPageObjArray.map((pageObj, index) => (
+          {FULL_PAGE_OBJ_ARRAY.map((pageObj, index) => (
             <section
               id={pageObj.name}
               key={pageObj.name}
