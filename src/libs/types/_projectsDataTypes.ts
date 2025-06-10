942c538581d05381a4dfeba09ab5af4046b1295a
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2025-06-10 13:02:01 +0900

    feat : project types & 오타수정

---
+++ src/libs/types/_projectsDataTypes.ts
@@ -0,0 +1,32 @@
+// project-data/types.ts
+
+export interface Project {
+  name: string;
+  team: string[] | string;
+  role: string;
+  period: string;
+  address: string;
+  gitrepo: string;
+  skills: string[];
+  pointDescriptions: string[];
+  logo: string;
+  textLogo?: string;
+  troubleshootings?: Troubleshooting[];
+  myJobs?: MyJob[];
+}
+
+export interface Troubleshooting {
+  title: string;
+  problemSituation: string;
+  process: {
+    littleTitle: string;
+    text: string;
+    more?: string;
+  }[];
+  result: string[];
+}
+
+export interface MyJob {
+  description: string;
+  img?: string;
+}
--- src/libs/types/index.ts
+++ src/libs/types/index.ts
@@ -1,3 +1,5 @@
 export * from "./_FullPageTypes";
-export * from "./_ThemeType"
+export * from "./_ThemeType";
 export * from "./_LeafData";
+export * from "./_projectsDataTypes";
+export * from "./_formValues";
