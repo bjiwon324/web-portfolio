df6776c11d2a9cfbdabb2c542b1501feaca477f3
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2025-06-10 13:06:16 +0900

    fix : convention

--- src/components/(domains)/home/Intro/index.tsx
+++ src/components/(domains)/home/Intro/index.tsx
@@ -1,24 +1,21 @@
-import styles from './intro.module.scss'
-import classNames from 'classnames/bind'
-import LeavesContainer from './leavesContainer';
+import styles from "./intro.module.scss";
+import classNames from "classnames/bind";
+import LeavesContainer from "./leavesContainer";
 
-
-
-const cx = classNames.bind(styles)
-
+const cx = classNames.bind(styles);
 
 export default function InTroSection() {
   return (
     <>
       <LeavesContainer>
-        <div className={cx('text-box')}>
-          <h1 className={cx('name')}>박지원</h1>
+        <div className={cx("text-box")}>
+          <h1 className={cx("name")}>박지원</h1>
           <div className={cx("job-line")}>
-            <p className={cx('job')}>Front-end</p>
-            <p className={cx('job')}>Desiner</p>
+            <p className={cx("job")}>Front-end</p>
+            <p className={cx("job")}>Designer</p>
           </div>
         </div>
       </LeavesContainer>
     </>
-  )
+  );
 }
